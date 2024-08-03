62c2c4798190f5559cecb5c3d1b0d75593f71bcc
LeonardoAdami21 <lolzinhobr1000@gmail.com>
2024-08-03 09:38:31 -0300

    feat/email, envios de emails e conselhos feito com sucesso.

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,8 +1,9 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Inject, Injectable } from '@nestjs/common';
 import { CreateEmailDto } from './dto/create.email.dto';
-import { EmailRepository } from './repositories/email.repository';
 import { EmailRepositoryInterface } from './repositories/email.repository.interface';
+import axios, { AxiosError } from 'axios';
+import { envoriment } from '../env/envoriment';
 
 @Injectable()
 export class EmailService {
@@ -13,15 +14,24 @@ export class EmailService {
   ) {}
 
   async create(dto: CreateEmailDto) {
-    const mail = {
-      to: dto.email,
-      from: 'modules@nestjs.com',
-      text: 'Testing',
-      subject: 'Testing Nest MailerModule ✔',
-      html: '<b>Welcome</b>',
-    };
-    await this.mailService.sendMail(mail);
-    const result = await this.emailRepository.create(dto);
-    return result;
+    try {
+      const conselho = await axios
+        .get(`${envoriment.conselhoUrl}`)
+        .then((res) => res.data.slip.advice)
+        .catch((error: AxiosError) => {
+          console.log(error);
+        });
+
+      const mail = {
+        to: dto.email,
+        from: 'modules@nestjs.com',
+        text: 'Conselho do dia',
+        subject: 'Testing Nest MailerModule ✔',
+        html: `<b>Bom dia o conselho do dia é : ${conselho} </b>`,
+      };
+      await this.mailService.sendMail(mail);
+      const result = await this.emailRepository.create(dto);
+      return result;
+    } catch (error) {}
   }
 }
